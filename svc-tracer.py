72eb8329386fdbfff56084f800023bc752059857
gebing <gebing@foxmail.com>
2020-09-24 17:39:53 +0800

    Initial commit.

---
+++ svc-tracer.py
@@ -0,0 +1,10 @@
+#!C:\Python27\python.exe
+# -*- coding: utf-8 -*-
+from svc_tracer.tracer import main
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
